da09cbfe075d3218c5f05cb18cbaa2bb60aff0fc
twichtendahl <traviswichtendahl@gmail.com>
2019-08-14 17:57:34 -0700

    we see order details

--- app/src/main/java/com/example/restaurant_receiver_3/MainActivity.java
+++ app/src/main/java/com/example/restaurant_receiver_3/MainActivity.java
@@ -8,6 +8,10 @@ import android.widget.TextView;
 
 import com.example.datamodel.Payment;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
 
     Payment payment;
@@ -15,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     TextView creditCardConfirm;
     TextView cscConfirm;
     TextView expiryConfirm;
+    TextView test1;
+    TextView test2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +32,36 @@ public class MainActivity extends AppCompatActivity {
         creditCardConfirm = findViewById(R.id.creditConfirm);
         cscConfirm = findViewById(R.id.cscConfirm);
         expiryConfirm = findViewById(R.id.expiryConfirm);
+        test1 = findViewById(R.id.test1);
+        test2 = findViewById(R.id.test2);
 
         Bundle bundle = getIntent().getExtras();
         String paymentAsString = getString(R.string.send_failed);
         if (bundle != null) {
             paymentAsString = bundle.getString("payment_string_key");
+            String orderDetail = paymentAsString.substring(0, paymentAsString.indexOf('*'));
+            String paymentDetail = paymentAsString.substring(paymentAsString.indexOf('*') + 1);
+
+            ArrayList<String> details = new ArrayList<>();
+            for(int i = 0; i < paymentDetail.length(); i++) {
+                String word = "";
+                char c = paymentDetail.charAt(i);
+                if(c != ' ') {
+                    word += c;
+                } else {
+                    details.add(word);
+                }
+            }
+
+            confirmationCode.setText(details.get(7));
+            creditCardConfirm.setText(details.get(2));
+            cscConfirm.setText(details.get(3));
+            expiryConfirm.setText(String.format("%s/%s", details.get(4), details.get(5)));
+
+            test1.setText(orderDetail);
+            test2.setText(paymentDetail);
         }
         // Populate with data from PaymentActivity
-        confirmationCode.setText(paymentAsString);
+        // confirmationCode.setText(paymentAsString);
     }
 }
